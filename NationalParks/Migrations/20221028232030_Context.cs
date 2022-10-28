4bc5f710f4004af99e281b1498abcc41ff135190
Shaniza Lingle <shanizalingle@gmail.com>
2022-10-28 16:22:33 -0700

    add Context migration

---
+++ NationalParks/Migrations/20221028232030_Context.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalParks.Migrations
+{
+    public partial class Context : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Cost", "Country", "Name", "State" },
+                values: new object[,]
+                {
+                    { 2, 35, "USA", "Yosemite National Park", "California" },
+                    { 3, 35, "USA", "Grand Canyon National Park", "Arizona" },
+                    { 4, 20, "USA", "Yellowstone National Park", "Wyoming" },
+                    { 5, 10, "Canada", "Jasper National Park", "Alberta" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 1,
+                column: "Description",
+                value: "Multiple lodges to choose from");
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "ServiceId", "Cost", "Description", "Name", "ParkId", "Type" },
+                values: new object[,]
+                {
+                    { 2, 80, "Boat, bus, raft and horseback tours available", "Guided Tours", 1, "Tours" },
+                    { 3, 40, "First come first serve, with overnight parking ", "Wilderness Camping", 1, "Camping" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "ServiceId", "Cost", "Description", "Name", "ParkId", "Type" },
+                values: new object[,]
+                {
+                    { 4, 120, "Tent cabins at  High Sierra Camps / deluxe rooms at The Ahwahnee", "Yosemite Lodging", 2, "Lodging" },
+                    { 5, 40, "Ranger-led programs may be available on a limited basis", "Ranger & Interpretive Programs", 2, "Program" },
+                    { 6, 90, "South Rim lodging is available all year", "South Rim Lodgine", 3, "Lodging" },
+                    { 7, 0, "23 mile scenic road between Desert View and Grand Canyon Village,", "Desert View", 3, "Scenery" },
+                    { 8, 0, "coffee bar, grab-and-go breakfast and Lunch, and hiker/biker supplies", "Grand Canyon Vistor Center", 3, "Amenities" },
+                    { 9, 30, "12 campgrounds with over 2,000 established campsites.", "Yosemite Campgrounds", 4, "Camping" },
+                    { 10, 0, "Hottest hot springs in the Canadian Rockies", "Miette Hot Springs", 5, "Scenery" },
+                    { 11, 140, "See elk, deer, bear, moose and mountain goats ", "Wildlife Tours", 5, "Tours" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Services",
+                keyColumn: "ServiceId",
+                keyValue: 1,
+                column: "Description",
+                value: "Our guest experience is characterized by warm, personalized service and convenient in-room amenities to make the most of your stay in Glacier National Park.");
+        }
+    }
+}
--- NationalParks/Models/NationalParksContext.cs
+++ NationalParks/Models/NationalParksContext.cs
@@ -12,11 +12,25 @@ namespace NationalParks.Models
         {
           builder.Entity<Service>()
             .HasData(
-              new Service { ServiceId = 1, Name = "Glacier National Park Lodges", Type = "Lodging", Description = "Our guest experience is characterized by warm, personalized service and convenient in-room amenities to make the most of your stay in Glacier National Park.", Cost = 100, ParkId = 1}
+              new Service { ServiceId = 1, Name = "Glacier National Park Lodges", Type = "Lodging", Description = "Multiple lodges to choose from", Cost = 100, ParkId = 1},
+              new Service { ServiceId = 2, Name = "Guided Tours", Type = "Tours", Description = "Boat, bus, raft and horseback tours available", Cost = 80, ParkId = 1},
+              new Service { ServiceId = 3, Name = "Wilderness Camping", Type = "Camping", Description = "First come first serve, with overnight parking ", Cost = 40, ParkId = 1},
+              new Service { ServiceId = 4, Name = "Yosemite Lodging", Type = "Lodging", Description = "Tent cabins at  High Sierra Camps / deluxe rooms at The Ahwahnee", Cost = 120, ParkId = 2},
+              new Service { ServiceId = 5, Name = "Ranger & Interpretive Programs", Type = "Program", Description = "Ranger-led programs may be available on a limited basis", Cost = 40, ParkId = 2},
+              new Service { ServiceId = 6, Name = "South Rim Lodgine", Type = "Lodging", Description = "South Rim lodging is available all year", Cost = 90, ParkId = 3},
+              new Service { ServiceId = 7, Name = "Desert View", Type = "Scenery", Description = "23 mile scenic road between Desert View and Grand Canyon Village,", Cost = 0, ParkId = 3},
+              new Service { ServiceId = 8, Name = "Grand Canyon Vistor Center", Type = "Amenities", Description = "coffee bar, grab-and-go breakfast and Lunch, and hiker/biker supplies", Cost = 0, ParkId = 3},
+              new Service { ServiceId = 9, Name = "Yosemite Campgrounds", Type = "Camping", Description = "12 campgrounds with over 2,000 established campsites.", Cost = 30, ParkId = 4},
+              new Service { ServiceId = 10, Name = "Miette Hot Springs", Type = "Scenery", Description = "Hottest hot springs in the Canadian Rockies", Cost = 0, ParkId = 5},
+              new Service { ServiceId = 11, Name = "Wildlife Tours", Type = "Tours", Description = "See elk, deer, bear, moose and mountain goats ", Cost = 140, ParkId = 5}
             );
           builder.Entity<Park>()
             .HasData(
-              new Park { ParkId = 1, Name = "Glacier National Park", State = "Montana", Country = "USA", Cost = 35}
+              new Park { ParkId = 1, Name = "Glacier National Park", State = "Montana", Country = "USA", Cost = 35},
+              new Park { ParkId = 2, Name = "Yosemite National Park", State = "California", Country = "USA", Cost = 35},
+              new Park { ParkId = 3, Name = "Grand Canyon National Park", State = "Arizona", Country = "USA", Cost = 35},
+              new Park { ParkId = 4, Name = "Yellowstone National Park", State = "Wyoming", Country = "USA", Cost = 20},
+              new Park { ParkId = 5, Name = "Jasper National Park", State = "Alberta", Country = "Canada", Cost = 10}
             );
         }
         public DbSet<Park> Parks { get; set; }
